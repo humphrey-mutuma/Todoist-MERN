c693380e8dedf7dde3b234d15bf2fc831c52d4c9
humphrey-mutuma <humphreymutuma01@gmail.com>
2022-06-25 16:35:59 +0300

    succesfully fetch data from backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,41 +1,13 @@
 const mongoose = require("mongoose");
 
-const MONGODB_URI = process.env.MONGO_URI;
-
-// check if we have a MONGODB URI
-if (!MONGODB_URI) {
-  throw new Error("Please define MONGODB_URI in the .env file");
-}
-
-// use global cashed for cashing in development to prevent connections from growing exponentially
-
-let cashed = global.mongoose;
-
-if (!cashed) {
-  cashed = global.mongoose = { conn: null, promise: null };
-}
-
 const connectDB = async () => {
-  if (cashed.conn) {
-    return cashed.conn;
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
   }
-
-  if (!cashed.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cashed.promise = await mongoose
-      .connect(MONGODB_URI, opts)
-      .then((mongoose) => {
-        return mongoose;
-      });
-  }
-  cashed.conn = await cashed.promise;
-  return (
-    cashed.conn,
-    console.log(`MongoDB connected: ${cashed.conn.connection.host}`.cyan)
-  );
 };
 
 module.exports = { connectDB };
